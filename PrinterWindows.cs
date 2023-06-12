aa042434f18d45cff5640e7d676706512967c10c
ardhel08 <ardeliacraft@gmail.com>
2023-06-12 16:37:19 +0700

    Polyomorphism

---
+++ PrinterWindows.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace polymorphism
+{
+    public class PrinterWindows
+    {
+       public virtual void Print()
+        {
+            Console.WriteLine("Printer Windows printing...");
+        }
+
+        public virtual void Show()
+        {
+            Console.WriteLine("Printer Windows display dimension : 10*11");
+        }
+
+    }
+
+    public class Epson : PrinterWindows
+    {
+        public override void Print()
+        {
+            Console.WriteLine("Epson printer printing....");
+        }
+        public override void Show()
+        {
+            Console.WriteLine("Epson display dimension : 10*11");
+
+        }
+    }
+
+    public class Canon : PrinterWindows
+    {
+        public override void Print()
+        {
+            Console.WriteLine("Canon printer printing....");
+        }
+        public override void Show()
+        {
+            Console.WriteLine("Canon display dimension : 10*11");
+
+        }
+    }
+
+    public class LaserJet : PrinterWindows
+    {
+        public override void Print()
+        {
+            Console.WriteLine("LaserJet printer printing....");
+        }
+        public override void Show()
+        {
+            Console.WriteLine("LaserJet display dimension : 10*11");
+
+        }
+    }
+}
